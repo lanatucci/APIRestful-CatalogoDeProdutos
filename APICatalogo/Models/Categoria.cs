126cbfe4ababfca869b916efbe6ade9f0e4cacf3
unknown <lanatucci2016@gmail.com>
2025-09-22 09:57:21 -0300

    Swagger configurado

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace APICatalogo.Models;
+
+public class Categoria
+{
+    //por conveção o EF entende o nome Id no final ou sozinho como chave primária
+    public int CategoriaId { get; set; }
+
+    //Definindo a propriedade como nula -> ?
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+
+
+}
