e3c3c2f6ea2099d207b3b24d5d402055f6e8ec6b
unknown <lanatucci2016@gmail.com>
2025-10-09 11:36:35 -0300

    Controller

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,105 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        //adicionando instancia do contexto para acessar o banco
+        //readonly = somente leitura
+        //para tirar o verde do _context, senlecionar toa a linha e gerar construtor
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //IEnumerable = coleção de produtos, mais otimizado que usar List
+        //actionResult = permite retornar código de status http mesmo quando o metodo pede outro tipo de dado
+        //Sempre definir código de status de erro http
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if(produtos is null)
+            {
+                //metodo do ControllerBase  
+                return NotFound("Produtos não encontrados");
+            }
+            return produtos;
+        }
+
+        //definindo rota com parametro id do tipo int
+        [HttpGet ("{id:int}", Name ="ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if(produto is null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+                return BadRequest("nulo");
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto",
+                new { id = produto.ProdutoId
+                }, produto);
+        }
+
+        //atualização completa  do recurso
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+                return BadRequest("Produto não encontrado");
+
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok(produto);
+        }
+
+        //remoção do recurso
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok(produto);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- APICatalogo/obj/Release/net9.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Release/net9.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f173d419c0b39aa971cb00678879f5c307cb68e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56b00e49c4f4b7fecf9fab569bb16e2b416084b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
